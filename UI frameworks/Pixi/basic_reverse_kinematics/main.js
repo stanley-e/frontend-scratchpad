034c970f3e5140d00d56017d9628b5b5e748d55f
Stanley Elliott <5688759+stanley-e@users.noreply.github.com>
2021-05-07 18:44:32 +1000

    Add math & logic for determing the correct arm positions

--- UI frameworks/Pixi/basic_reverse_kinematics/main.js
+++ UI frameworks/Pixi/basic_reverse_kinematics/main.js
@@ -18,34 +18,32 @@ centerMarker.angle = 45
 app.stage.addChild(centerMarker)
 
 
+shoulderArm = new PIXI.Graphics()
+shoulderArm.lineStyle(4, 0x00ffff, 2)
+shoulderArmLength = 70
+shoulderArm.lineTo(0,-shoulderArmLength)
+shoulderArm.x = centerX
+shoulderArm.y = centerY
+app.stage.addChild(shoulderArm)
 
-upperArm = new PIXI.Graphics()
-upperArm.lineStyle(4, 0x00ffff, 2)
-upperArmLength = 70
-upperArm.lineTo(0,-upperArmLength)
-upperArm.x = centerX
-upperArm.y = centerY
-app.stage.addChild(upperArm)
-
-lowerArm = new PIXI.Graphics()
-lowerArm.lineStyle(4, 0xff00ff,2)
-lowerArmLength = 50
-lowerArm.lineTo(0,-lowerArmLength)
-lowerArm.x = 0
-lowerArm.y = -upperArmLength
-upperArm.addChild(lowerArm)
-
+elbowArm = new PIXI.Graphics()
+elbowArm.lineStyle(4, 0xff00ff,2)
+elbowArmLength = 50
+elbowArm.lineTo(0,-elbowArmLength)
+elbowArm.x = 0
+elbowArm.y = -shoulderArmLength
+shoulderArm.addChild(elbowArm)
 
 upperRing = new PIXI.Graphics()
 upperRing.lineStyle(1, 0xffffff, 1)
-upperRing.drawCircle(0,0, upperArmLength + lowerArmLength)
+upperRing.drawCircle(0,0, shoulderArmLength + elbowArmLength)
 upperRing.x = centerX
 upperRing.y = centerY
 app.stage.addChild(upperRing)
 
 lowerRing = new PIXI.Graphics()
 lowerRing.lineStyle(1, 0xffffff,1)
-lowerRing.drawCircle(0,0, Math.abs(upperArmLength - lowerArmLength))
+lowerRing.drawCircle(0,0, Math.abs(shoulderArmLength - elbowArmLength))
 lowerRing.x = centerX
 lowerRing.y = centerY
 app.stage.addChild(lowerRing)
@@ -55,6 +53,56 @@ pointMarker.lineStyle(1, 0xAA0000, 1)
 pointMarker.drawCircle(0,0,4)
 markerUsed = false;
 
+function distance_between_points(aX,aY,bX,bY) {
+	 return Math.sqrt((aX-bX)**2 + (aY-bY)**2)
+}
+
+function sss_formula(leftSide,rightSide,oppositeSide) {
+	// Given three side lengths of a triangle calculates the interior angle in degrees
+	// See https://www.mathsisfun.com/algebra/trig-solving-sss-triangles.html
+	// Note can return NaN if a solution is not possible
+	numerator = leftSide**2 + rightSide**2 - oppositeSide**2
+	denominator = 2 * leftSide * rightSide
+	radiansAngle = Math.acos(numerator / denominator)
+	degreesAngle = radiansAngle * PIXI.RAD_TO_DEG
+	return degreesAngle
+}
+
+
+function simplifyDegrees(angle) {
+	angle = angle % 360
+	if (angle < 0) {
+		angle = angle + 360
+	}
+	return angle
+}
+
+function angleDiffDegrees(startAngle,stopAngle) {
+	// Returns the smallest change in degrees that when added to the start angle
+	// results.
+	// Eg 350 to 20 is +10 degrees
+	// 20 to 350 is -10 degrees
+	// 725 to 0 is -5 degrees
+	// If stopAngle is completely opposite startAngle then position is returned
+	startAngle = simplifyDegrees(startAngle)
+	stopAngle = simplifyDegrees(stopAngle)
+
+	if (stopAngle >= startAngle) {
+		clockwiseDiff = stopAngle - startAngle
+		anticlockwiseDiff = -(360 - clockwiseDiff)
+	} else {
+		clockwiseDiff = (360 - startAngle) + stopAngle
+		anticlockwiseDiff = -(startAngle - stopAngle)
+	}
+	if (clockwiseDiff <= 180) {
+		return clockwiseDiff
+	} else {
+		return anticlockwiseDiff
+	}
+}
+
+
+	
 
 app.renderer.plugins.interaction.on('mousedown', function(ev) {
 	mouseDownX = ev.data.global.x;
@@ -64,6 +112,33 @@ app.renderer.plugins.interaction.on('mousedown', function(ev) {
 	}
 	pointMarker.x = mouseDownX
 	pointMarker.y = mouseDownY
+
+	pointDistance = distance_between_points(centerX,centerY,mouseDownX,mouseDownY)
+	pointAngleRadians = Math.atan2(centerY-mouseDownY, centerX-mouseDownX)
+	pointAngleDegrees = PIXI.RAD_TO_DEG * pointAngleRadians
+	console.log(pointAngleDegrees)
+
+	baseTargetShoulderAngle = pointAngleDegrees - 90
+	ShoulderShiftAngle = sss_formula(shoulderArmLength,pointDistance,elbowArmLength)
+	elbowShiftAngle = sss_formula(shoulderArmLength,elbowArmLength,pointDistance)
+
+	leftTargetShoulderAngle = baseTargetShoulderAngle - ShoulderShiftAngle
+	rightTargetShoulderAngle = baseTargetShoulderAngle + ShoulderShiftAngle
+
+	leftDiff = angleDiffDegrees(shoulderArm.angle, leftTargetShoulderAngle)
+	rightDiff = angleDiffDegrees(shoulderArm.angle, rightTargetShoulderAngle)
+
+	if (Math.abs(leftDiff) <= Math.abs(rightDiff)) {
+		targetShoulderAngle = leftTargetShoulderAngle
+		targetElbowAngle =  180 - elbowShiftAngle 
+	} else {
+		targetShoulderAngle = rightTargetShoulderAngle
+		targetElbowAngle = 180 + elbowShiftAngle
+	}
+
+
+	shoulderArm.angle = targetShoulderAngle
+	elbowArm.angle = targetElbowAngle
 })
 
 
